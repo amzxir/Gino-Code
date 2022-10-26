b529596ecbf5d4de236bd4f1761816dea22fea96
amirreza79ahmadi <amirreza79ahmadi@gmail.com>
2022-10-26 14:37:39 +0330

    team

--- src/component/home/team/card.jsx
+++ src/component/home/team/card.jsx
@@ -1,13 +1,127 @@
 import styled from "styled-components";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Cards = styled.div({
+    flex:'0 0 auto',
+    width:'23%',
 
+    '@media (max-width: 992px)': {
+        width:'45%',
+        marginBottom:'1rem',
+    },
+
+    '@media (max-width: 500px)': {
+        width:'100%',
+        marginBottom:'1rem',
+    },
+    
+    '&> div.col3':{
+
+        '&> div.member':{
+            borderRadius:'8px',
+            borderTop:'5px solid #CDAC81',
+            boxShadow:'0 0.5rem 1rem rgb(0 0 0 / 15%)',
+            transition:'150ms',
+
+            '&:hover': {
+                borderColor:'#FF0F10'
+            },
+
+            '&> div.p4':{
+
+                padding:'1.5rem',
+
+                '&> div.imgCenter':{
+                    display:'flex',
+                    justifyContent:'center',
+                    marginBottom:'1.5rem',
+    
+                    '& img':{
+                        maxWidth:'100%',
+                        height:'auto',
+                        borderRadius:'50%',
+
+                        '@media (max-width: 500px)': {
+                            width:'200px',
+                            height:'200px',
+                        },
+                    }
+                },
+    
+                '&> div.content':{
+                    textAlign:'center',
+                    '& h2':{
+                        fontSize:'18px',
+                        marginBlockStart:'0'
+                    },
+
+                    '& p':{
+                        fontSize:'12px',
+                        fontWeight:'600',
+                        color:'#5A5A5A',
+                        marginBlockStart:'0',
+
+                    }
+                },
+            },
+
+            
+
+            '&> div.media':{
+                padding:'1rem',
+                backgroundColor:'#CDAC81',
+                display:'flex',
+                justifyContent:'center',
+                borderEndEndRadius:'8px',
+                borderEndStartRadius:'8px',
+                '&> a':{
+                    padding:'0',
+                    display:'flex',
+                    alignItems:'center',
+                    justifyContent:'center',
+                    fontWeight:'normal',
+                    borderRadius:'8px',
+                    width:'38px',
+                    height:'38px',
+                    color:'#CDAC81',
+                    backgroundColor:'#fff',
+                    margin:'0px 8px 0px 8px',
+
+                    '&:hover': {
+                        color:'#5A5A5A'
+                    }
+                }
+            }
+
+        }
+
+    }
 })
 
-const Card = () => {
+const Card = (props) => {
+
     return ( 
         <Cards>
-            
+            <div className="col3">
+                <div className="member">
+                    <div className="p4">
+                        <div className="imgCenter">
+                            <img src={props.images} alt="" />
+                        </div>
+                        <div className="content">
+                            <h2>{props.name}</h2>
+                            <p>{props.profession}</p>
+                        </div>
+                    </div>
+                    <div className="media">
+                        {props.media.map((i , index)=> {
+                            return(
+                                <a key={index} href={i.url}><FontAwesomeIcon icon={i.icon}/></a>
+                            )
+                        })}
+                    </div>
+                </div>
+            </div>
         </Cards>
     );
 }
--- src/component/home/team/team.jsx
+++ src/component/home/team/team.jsx
@@ -1,5 +1,11 @@
 import styled from "styled-components";
 import Card from "./card";
+import images1 from './images/team-1.jpeg'
+import images2 from './images/team-2.jpeg'
+import images3 from './images/team-3.jpeg'
+import images4 from './images/team-4.jpeg'
+import { faInstagram , faTwitter , faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+
 
 const Teams = styled.div({
     padding:'0px 4rem 0px 4rem',
@@ -25,7 +31,11 @@ const Teams = styled.div({
         }
     },
 
-    '&> div.cardTeam':{
+    '&> div.row':{
+        display:'flex',
+        flexDirection:'row',
+        justifyContent:'space-around',
+        flexWrap:'wrap',
         
     }
 })
@@ -35,6 +45,37 @@ const dataContent = {
     des:'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است .'
 }
 
+const data = [
+    {id:1 , images:images1 , name:'امیر احمدی' , profession:'برنامه نویس' , 
+    media:[
+        {url:'#' , icon:faInstagram},
+        {url:'#' , icon:faTwitter},
+        {url:'#' , icon:faLinkedinIn},
+    ],
+    },
+    {id:2 , images:images2 , name:'لورم ایپسوم' , profession:'برنامه نویس',
+    media:[
+        {url:'#' , icon:faInstagram},
+        {url:'#' , icon:faTwitter},
+        {url:'#' , icon:faLinkedinIn},
+    ],
+    },
+    {id:3 , images:images3 , name:'پیمان تیموریان' , profession:'برنامه نویس',
+    media:[
+        {url:'#' , icon:faInstagram},
+        {url:'#' , icon:faTwitter},
+        {url:'#' , icon:faLinkedinIn},
+    ],
+    },
+    {id:4 , images:images4 , name:'لورم ایپسوم' , profession:'برنامه نویس' , 
+    media:[
+        {url:'#' , icon:faInstagram},
+        {url:'#' , icon:faTwitter},
+        {url:'#' , icon:faLinkedinIn},
+    ],
+    }
+]
+
 const Team = () => {
     return ( 
         <Teams>
@@ -42,8 +83,20 @@ const Team = () => {
                 <h1 className="contentAfter">{dataContent.title}</h1>
                 <p>{dataContent.des}</p>
             </div>
-            <div className="cardTeam">
-                <Card/>
+            <div className="row">
+
+                    {data.map((i , index)=> {
+                        return(
+                            <Card
+                            key={index}
+                            id={i.id}
+                            images={i.images}
+                            name={i.name}
+                            profession={i.profession}
+                            media={i.media}
+                            />
+                        )
+                    })}
             </div>
         </Teams>
     );
