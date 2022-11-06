50d54430e50cb6c52b905a23d7efabbb986d14d6
amirreza79ahmadi <amirreza79ahmadi@gmail.com>
2022-11-06 18:02:14 +0330

    debug modal

--- src/component/modal/modal.jsx
+++ src/component/modal/modal.jsx
@@ -1,6 +1,12 @@
+import { useContext } from "react";
+import Context from "../../context/context";
+
 const Modal = (props) => {
+
+    const {modal , setModal} = useContext(Context)
+
     return ( 
-        <div className={!props.show?'dNone':'openModal dBlock'}>
+        <div className={!props.show?'dNone':'openModal dBlock'} onClick={()=> setModal(true)}>
             {
             props.children
             }
